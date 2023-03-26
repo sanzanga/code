173c5e79943a3f9a6f22df951cdedda405a027ad
sanzanga <92855561+sanzanga@users.noreply.github.com>
2023-03-26 18:05:26 +0800

    零钱通项目更改
    零钱通项目面向对象实现，测试类

---
+++ Java/Java基础/smallchange/SmallChangeSysApp.java
@@ -0,0 +1,9 @@
+package com.smallchange;
+
+// 面向对象编程
+// 调用相关方法
+public class SmallChangeSysApp {
+    public static void main(String[] args) {
+        new SmallChangeSysOpp().mainMenu();
+    }
+}
